f8fa0b345b7f5aa3af59c4dd813741332a282eca
Luis Fernando Gutiérrez Romo <luisromo@iles.com>
2017-06-15 23:31:37 -0500

    Lab01 y Lab02

---
+++ Lab02/AndroidApp/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private async void Validate()
+        {
+            SALLab02.ServiceClient ServiceClient = new SALLab02.ServiceClient();
+            string StudentEmail = "is696372@iteso.mx";
+            string Password = "23agosto";
+            string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            SALLab02.ResultInfo Result = await ServiceClient.ValidateAsync(StudentEmail, Password, myDevice);
+            Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+            Alert.SetTitle("Resultado de la verificación");
+            Alert.SetIcon(Resource.Drawable.Icon);
+            Alert.SetMessage(
+                $"{Result.Status}\n{Result.Fullname}\n{Result.Token}");
+            Alert.SetButton("Ok", (s, ev) => { });
+            Alert.Show();
+        }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Validate();
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
